e34700243cac72ff91b32c74da5b801c9d165811
Brian <thinhle1199@gmail.com>
2023-06-23 09:21:23 +0700

    test

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ function chooseSlide(n) {
 // 		topNav.className = "navbar";
 // 	}
 // }
-
 const formOpenBtn = document.querySelector("#form-open"),
   home = document.querySelector(".home"),
   formContainer = document.querySelector(".form_container"),
